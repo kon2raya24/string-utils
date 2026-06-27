13efbab2628b4d484507cf02288594e9cf13782a
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:02:25 +0000

    docs: add JSDoc comments to padright.ts

--- src/padright.ts
+++ src/padright.ts
@@ -1,3 +1,9 @@
+/**
+ * padRight
+ * @param str
+ * @param length
+ * @param char = " "
+ */
 export function padRight(str: string, length: number, char = " "): string {
   return str.padEnd(length, char);
 }
